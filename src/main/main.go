dbd53737ef23bb84cef82f8fb28d81e5b9c55eec
SpongyBacon <spongybacon@hotmail.co.uk>
2016-10-11 01:02:47 +0100

    json

--- src/main/main.go
+++ src/main/main.go
@@ -4,21 +4,36 @@ import (
     "net/http"
     "fmt"
     "encoding/json"
+    "../route"
 )
 
+const API_VERSION = "1"
+
+type abstractResponse struct {
+    Error   bool        `json:"error"`
+    Version string      `json:"version"`
+    Content interface{} `json:"content"`
+}
+
 func main() {
-    controller := NewRouteController()
-    controller.ErrorHandler(func(writer http.ResponseWriter, request *http.Request, status int, body string) {
-        response := struct {
-            Error   bool    `json:"error"`
-            Status  int     `json:"status"`
-            Message string  `json:"message"`
-        }{true, status, body}
-        json.NewEncoder(writer).Encode(response)
-    })
-    controller.Register("/", func(writer http.ResponseWriter, request *http.Request) {
-        fmt.Fprintln(writer, "Hello, world!")
-    })
+    controller := route.NewRouteController()
+    controller.ErrorHandler(errorHandler)
+    registerRoutes(controller)
     http.HandleFunc("/", controller.Handle)
     http.ListenAndServe(":8080", nil)
+    fmt.Println("Server started!")
+}
+
+func registerRoutes(controller *route.RouteController) {
+    controller.Register("/", indexRoute)
+}
+
+func errorHandler(writer http.ResponseWriter, _ *http.Request, status int, body string) {
+    response := abstractResponse{true, API_VERSION, body}
+    writer.WriteHeader(status)
+    json.NewEncoder(writer).Encode(response)
+}
+
+func indexRoute(writer http.ResponseWriter, _ *http.Request) {
+    json.NewEncoder(writer).Encode(abstractResponse{false, API_VERSION, "Hello, world!"})
 }
